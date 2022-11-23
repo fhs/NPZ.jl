bc3a8c680823117068f997f603eafded10380746
Carlo Lucibello <carlo.lucibello@gmail.com>
2022-11-23 04:35:43 +0100

    update project and workflows (#57)
    * update project and workflows
    
    * less CI
    
    * set arch
    
    * cleanup
    
    * bump julia version

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ DocMeta.setdocmeta!(NPZ, :DocTestSetup, :(using NPZ); recursive=true)
 makedocs(;
     modules=[NPZ],
     repo="https://github.com/fhs/NPZ.jl/blob/{commit}{path}#L{line}",
-    sitename="NPZ",
+    sitename="NPZ.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://fhs.github.io/NPZ.jl",
